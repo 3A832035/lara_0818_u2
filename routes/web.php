eae71a6375c49574231beeedaba5b81c26d80488
3A832035 <3a832035@gm.student.ncut.edu.tw>
2021-11-02 02:19:36 +0800

    HomeController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('home', function () {
-    return view('home.index');
-});
+Route::get('home',[HomeController::class, 'index']);
